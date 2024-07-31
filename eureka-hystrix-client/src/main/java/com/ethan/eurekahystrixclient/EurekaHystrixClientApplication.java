87fedbb10147d8165729c14c783186c1bc8c40ac
Ethan519013 <ethan519013@gmail.com>
2024-07-31 10:11:13 +0800

    .gitignore

---
+++ eureka-hystrix-client/src/main/java/com/ethan/eurekahystrixclient/EurekaHystrixClientApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.eurekahystrixclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaHystrixClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaHystrixClientApplication.class, args);
+    }
+
+}
